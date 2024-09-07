a085ad8964bbe36545360a75c48ff1554f6f0331
Edson Y. Toma <edson.yudi.toma@gmail.com>
2024-09-07 14:28:05 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SAMPLE_DIR="ProjectContent"
+GITHUB_REPO_NAME="iOSProjectSetup"
+GITHUB_REPO_URL="https://github.com/EdYuTo/iOSProjectSetup.git"
 PROJECT_NAME_CONSTANT="__PROJECTNAMEIDENTIFIER__"
 BUNDLE_PREFIX_CONSTANT="__PROJECTBUNDLEPREFIX__"
 DEVELOPMENT_TEAM_CONSTANT="__PROJECTDEVELOPMENTTEAM__"
@@ -17,17 +19,28 @@ echo "What's your development team id? You can leave this blank"
 echo "or find it here https://developer.apple.com/account under 'Membership details'"
 read DEVELOPMENT_TEAM_INPUT
 
-find $SAMPLE_DIR -name "*$PROJECT_NAME_CONSTANT*" -type d | while read directory; do
+if [ -d $PROJECT_NAME_INPUT ]; then
+  echo "Error: Project already exists in this directory, try choosing a new project name"
+  exit -1
+fi
+
+git clone --no-checkout $GITHUB_REPO_URL $GITHUB_REPO_NAME && cd $GITHUB_REPO_NAME
+git sparse-checkout init --no-cone && git sparse-checkout set $SAMPLE_DIR
+git checkout main
+mv $SAMPLE_DIR ../$PROJECT_NAME_INPUT
+cd ../ && rm -rf iOSProjectSetup
+
+find $PROJECT_NAME_INPUT -name "*$PROJECT_NAME_CONSTANT*" -type d | while read directory; do
     newfile=$(echo "$directory" | sed "s/$PROJECT_NAME_CONSTANT/$PROJECT_NAME_INPUT/g")
     mv "$directory" "$newfile"
 done
 
-find $SAMPLE_DIR -name "*$PROJECT_NAME_CONSTANT*" -type f | while read file; do
+find $PROJECT_NAME_INPUT -name "*$PROJECT_NAME_CONSTANT*" -type f | while read file; do
     newfile=$(echo "$file" | sed "s/$PROJECT_NAME_CONSTANT/$PROJECT_NAME_INPUT/g")
     mv "$file" "$newfile"
 done
 
-find $SAMPLE_DIR -type f | while read file; do
+find $PROJECT_NAME_INPUT -type f | while read file; do
     newfile=$(sed "s/$PROJECT_NAME_CONSTANT/$PROJECT_NAME_INPUT/g" $file)
     newfile=$(echo "$newfile" | sed "s/$BUNDLE_PREFIX_CONSTANT/$BUNDLE_PREFIX_INPUT/g")
     newfile=$(echo "$newfile" | sed "s/$DEVELOPMENT_TEAM_CONSTANT/$DEVELOPMENT_TEAM_INPUT/g")
